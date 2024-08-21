4cb1608912d70fa86ab9f9a46c46f482336b4c4d
samrocker <samrockerislive04@gmail.com>
2024-08-21 09:20:14 +0530

    header responsive fix design size fixed

--- src/components/LandingPage/HeroSection.tsx
+++ src/components/LandingPage/HeroSection.tsx
@@ -5,21 +5,21 @@ const HeroSection = () => {
   return (
     <section>
       <main className="max-w-[1440px] m-auto">
-        <div className="py-5 px-5 w-full flex-between flex-col lg:flex-row gap-10 lg:gap-0">
+        <div className="py-5 px-5 w-full flex-between flex-col lg:flex-row gap-10">
           <div className="flex-[1] flex w-full flex-col gap-5 items-start justify-center">
-            <span className="py-2 px-5 text-primary text-xs md:text-sm font-normal border-2 border-primary bg-primary/20 rounded-full">
+            <span className="py-2 px-5 text-primary text-xs md:text-sm lg:text-lg font-semibold border-2 border-primary bg-primary/20 rounded-full">
               Hey I'am
             </span>
             <div className="flex flex-col">
-              <h1 className="text-primary text-4xl md:text-5xl lg:text-7xl font-extrabold">
+              <h1 className="text-primary text-4xl md:text-5xl lg:text-8xl font-extrabold">
                 SAMRIDH
               </h1>
-              <h1 className="text-primary text-4xl md:text-5xl lg:text-7xl font-extrabold">
+              <h1 className="text-primary text-4xl md:text-5xl lg:text-8xl font-extrabold">
                 SATNALIKA
               </h1>
             </div>
             <div className="flex flex-col">
-              <p className="text-sm text-primary/75 font-normal max-w-[450px]">
+              <p className="text-sm lg:text-lg text-primary dark:text-primary/75 font-semibold max-w-[550px]">
                 <span className="underline">Full-Stack Web Developer</span> with 4+ years of experience, crafting
                 robust applications using Next.js with TypeScript and Node.js.
                 Let's bring your ideas to life with clean, efficient code.
@@ -27,7 +27,7 @@ const HeroSection = () => {
             </div>
             
           </div>
-          <div className="flex-[1] flex-center w-full gap-5">
+          <div className="flex-[1] flex-end w-full gap-5">
             <Image src='/images/myPicture.JPG' alt="Samridh" width={1000} height={1000} className="w-full max-w-[550px] rounded-3xl border-2 border-primary" />
           </div>
         </div>
--- src/components/common/HeaderSection.tsx
+++ src/components/common/HeaderSection.tsx
@@ -18,14 +18,14 @@ const HeaderSection = () => {
       <main className="max-w-[1440px] m-auto">
         <div className="py-5 px-5 w-full flex-between">
           <div className="flex-[1] flex-start">
-            <h1 className="text-primary text-lg md:text-2xl font-bold">SAM</h1>
+            <h1 className="text-primary text-xl md:text-2xl font-bold">AYO_SAM</h1>
           </div>
           <div className="flex-[1] hidden lg:flex-center gap-7">
             {HeaderContent.map((item, key) => (
               <div className="flex-center gap-5" key={key}>
                 <Link
                   href={item.path}
-                  className="text-primary text-lg font-normal"
+                  className="text-primary text-lg font-bold"
                 >
                   {item.title}
                 </Link>
@@ -34,7 +34,7 @@ const HeaderSection = () => {
           </div>
           <div className="flex-[1] hidden lg:flex-end gap-5">
             <div className="flex-center gap-3">
-              <h1 className="text-primary text-lg font-normal uppercase">
+              <h1 className="text-primary text-lg font-bold uppercase">
                 theme
               </h1>
               <ThemeChangerButton />
@@ -55,17 +55,44 @@ const HeaderSection = () => {
               />
             </Link>
           </div>
-          <div className="flex-[1] flex-end lg:hidden">
+          <div className="flex-[1] flex-end lg:hidden gap-5">
+              <ThemeChangerButton />
             <Sheet>
-              <SheetTrigger>Open</SheetTrigger>
-              <SheetContent>
-                <SheetHeader>
-                  <SheetTitle>Are you absolutely sure?</SheetTitle>
-                  <SheetDescription>
-                    This action cannot be undone. This will permanently delete
-                    your account and remove your data from our servers.
-                  </SheetDescription>
-                </SheetHeader>
+              <SheetTrigger asChild>
+                <Image src='/icons/menu-line.svg' width={27} height={27} alt="" className="dark:invert" />
+              </SheetTrigger>
+              <SheetContent side="left">
+                <div className="h-screen w-full flex gap-10 flex-col items-end justify-center">
+                  <div className="flex-center flex-col gap-7">
+                    {HeaderContent.map((item, key) => (
+                      <div className="flex-end gap-5" key={key}>
+                        <Link
+                          href={item.path}
+                          className="text-primary text-3xl font-normal"
+                        >
+                          {item.title}
+                        </Link>
+                      </div>
+                    ))}
+                  </div>
+                  <div className="flex-end flex-col gap-5">
+                    <Link
+                      href="/"
+                      className="py-3 px-5 flex-center gap-2 border-2 border-primary rounded-full backdrop-blur-lg bg-primary/20"
+                    >
+                      <h1 className="text-sm text-primary font-extrabold uppercase">
+                        Resume
+                      </h1>
+                      <Image
+                        src="/icons/download-line.svg"
+                        width={20}
+                        height={20}
+                        alt="download icon"
+                        className="dark:invert"
+                      />
+                    </Link>
+                  </div>
+                </div>
               </SheetContent>
             </Sheet>
           </div>
